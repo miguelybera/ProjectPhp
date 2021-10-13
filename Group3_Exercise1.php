b0ecf4a4519f73304fce98e2e9d63e5d6f6b5ae5
grnsthyn <granausthyn.devera.iics@ust.edu.ph>
2021-10-13 12:31:06 +0800

    Update Group3_Exercise1.php

--- Group3_Exercise1.php
+++ Group3_Exercise1.php
@@ -1,75 +1,107 @@
 <html>
-    <head>
-        <title>Exercise 1 Group 3</title>
-    </head>
 
-    <body>
-        <h1>Exercise 1 Group 3</h1>
-        <?php
+<head>
+    <title>Exercise 1 Group 3</title>
+</head>
 
-        #Abstraction
-        abstract class Admin{
+<body>
+    <h1>Exercise 1 Group 3</h1>
+    <?php
 
-            public abstract function view();
+    #Abstraction
+    abstract class Admin
+    {
+
+        public abstract function view();
+    }
+
+    #Inheritance
+    class Property extends Admin
+    {
+
+        #override $val parent class
+        public $val = "property";
+
+        public $propertyName;
+        private $propertyDescription;
+        private $propertyLocation;
+        private $propertyLotArea;
+        private $propertyType;
+        private $propertyPrice;
+        private $propertyLot = array();
+        public function view()
+        {
+            for ($i = 0; $i < count($this->propertyLot); $i++) {
+                print_r($this->propertyLot[$i] . "<br/>");
+            }
         }
 
-        #Inheritance
-        class Property extends Admin{
-
-            #override $val parent class
-            public $val = "property";
-
-            private $propertyName;
-            private $propertyDescription;
-            private $propertyLocation;
-            private $propertyLotArea;
-            private $propertyType;
-            private $propertyPrice;
-            private $propertyLot = array();
-            public function view(){
-                for($i = 0; $i<count($this->propertyLot); $i++){
-                    print_r($this->propertyLot[$i] . "<br/>");
-                }
-            }
-            public function createProperty($property){
-                $this->propertyLot[] = $property;
+        public function createProperty($property)
+        {
+            $this->propertyLot[] = $property;
         }
-            public function setPropertyValues($name, $location, $price, $description, $lotArea, $pType){
-                $this->propertyName = $name;
-                $this->propertyLocation = $location;
-                $this->propertyPrice = $price;
-                $this->propertyDescription = $description;
-                $this->propertyLotArea = $lotArea;
-                $this->propertyType = $pType;
-            }
-        
-            public function addProperty(){
-                $newProperty = "Name: ". $this->propertyName . "<br/>" ."Description: ". $this->propertyDescription . "<br/>" ." location: ". $this->propertyLocation .  "<br/>" ."Lot Area: ". $this->propertyLotArea . "<br/>" . "Type: ". $this->propertyType . "<br/>" ." price: ". $this->propertyPrice ."<br/>";
-                $this->createProperty($newProperty);
-            }
-            }
-            $miguel = new Property();
-            $miguel->setPropertyValues('House in Greenheights', 'Sucat Paranaque', '9 million', 'This is a 7 yr old house', '25 sqm', 'Townhouse');
-            $miguel->addProperty();
-            $miguel->setPropertyValues('House in Twinville', 'Marikina', '10 million', 'This is a 57 yr old house', '225 sqm', 'Mansion');
-            $miguel->addProperty();
-            $miguel->view();
+        public function setPropertyValues($name, $location, $price, $description, $lotArea, $pType)
+        {
+            $this->propertyName = $name;
+            $this->propertyLocation = $location;
+            $this->propertyPrice = $price;
+            $this->propertyDescription = $description;
+            $this->propertyLotArea = $lotArea;
+            $this->propertyType = $pType;
+        }
 
-            class Unit extends Property{
+        public function addProperty()
+        {
+            $newProperty = "Name: " . $this->propertyName . "<br/>" . "Description: " . $this->propertyDescription . "<br/>" . " location: " . $this->propertyLocation .  "<br/>" . "Lot Area: " . $this->propertyLotArea . "<br/>" . "Type: " . $this->propertyType . "<br/>" . " price: " . $this->propertyPrice . "<br/>";
+            $this->createProperty($newProperty);
+        }
 
-            #override $val child class
-            public $val = "unit";
-            
-            }
+        public function displayA($var)
+        {
+            #overriding #2 
+            echo "This is example of overriding in PROPERTY class";
+            echo $var->propertyName . '<br>';
+            echo $var->propertyDescription . '<br>';
+            echo $var->propertyLocation . '<br>';
+            echo $var->propertyLotArea . '<br>';
+            echo $var->propertyType . '<br>';
+            echo $var->propertyPrice . '<br>';
+        }
+    }
 
-            $gran = new Unit();
-           
-            #override print 
-            echo $miguel->val . '<br>';
-            echo $gran->val . '<br>';
-            
+    class Unit extends Property
+    {
+        #override $val child class
+        public $val = "This is example of overriding in UNIT Class";
 
-               
-        ?>
-    </body>
+        public function displayA($var)
+        {
+            #overriding #2 
+            echo $var->val . '<br>';
+        }
+    }
+
+
+    $miguel = new Property();
+    $miguel->setPropertyValues('House in Greenheights', 'Sucat Paranaque', '9 million', 'This is a 7 yr old house', '25 sqm', 'Townhouse');
+    $miguel->addProperty();
+    $miguel->setPropertyValues('House in Twinville', 'Marikina', '10 million', 'This is a 57 yr old house', '225 sqm', 'Mansion');
+    $miguel->addProperty();
+    $miguel->view();
+
+    $miguel->displayA($miguel);
+
+    echo '<br>';
+
+
+    $gran = new Unit();
+
+    $gran->displayA($gran);
+
+
+
+
+    ?>
+</body>
+
 </html>
